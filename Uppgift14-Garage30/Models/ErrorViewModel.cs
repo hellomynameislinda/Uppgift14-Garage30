84cce86ae0d26abbf427def2dd94ef2937b65e9a
Linda <linda@hellomynameislinda.com>
2024-05-02 09:41:45 +0200

    Add project files.

---
+++ Uppgift14-Garage30/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Uppgift14_Garage30.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
