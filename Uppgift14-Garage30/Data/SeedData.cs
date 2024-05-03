a3b5f4698e6773e9adf8033cefb9036570aae5fc
Linda <linda@hellomynameislinda.com>
2024-05-03 12:18:18 +0200

    Started setting up files for seed data

---
+++ Uppgift14-Garage30/Data/SeedData.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+
+namespace Uppgift14_Garage30.Data
+{
+    public class SeedData
+    {
+
+        private static Faker faker;
+
+        public static async Task InitSeedData(Uppgift14_Garage30Context context)
+        {
+//            if (await context.Member.AnyAsync()) return;
+        }
+    }
+}
