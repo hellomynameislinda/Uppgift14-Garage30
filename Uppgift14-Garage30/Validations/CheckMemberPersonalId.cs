1a2ba1abe71e90ac339eb4b83fe9a07e82afc2b9
Eugenia <musat.eugenia@gmail.com>
2024-05-06 14:06:06 +0200

    Revert "Add button and placeholder to Create view"
    This reverts commit 458a6ee9d3766ecb1dabb4e6523ca975277ad462.

--- Uppgift14-Garage30/Validations/CheckMemberPersonalId.cs
+++ Uppgift14-Garage30/Validations/CheckMemberPersonalId.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Uppgift14_Garage30.Data;
+using Uppgift14_Garage30.Models;
 
 namespace Uppgift14_Garage30.Validations
 {
@@ -8,11 +9,8 @@ namespace Uppgift14_Garage30.Validations
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
             var context = validationContext.GetRequiredService<Uppgift14_Garage30Context>();
-
             const string errorMessage = "Your Personal Id is registered already";
-            const string errorMessageFormatNumber = "Your Personal Id number must contain 10 numbers.";
-            const string errorMessageFormatChar = "Your Personal Id number must contain just numbers and no characters";
-            const string errorMessageLength = "Your Personal Id number must not exceed 10 digits";
+            const string errorMessageFormat = "Your Personal Id number must contain 10 numbers";
 
             if (value is string input && !string.IsNullOrEmpty(input))
             {
@@ -21,19 +19,10 @@ namespace Uppgift14_Garage30.Validations
                 {
                     return new ValidationResult(errorMessage);
                 }
-                if (input.Any(char.IsLetter))
+                if(input.Length != 10 && input.All(char.IsDigit))
                 {
-                    return new ValidationResult(errorMessageFormatChar);
+                    return new ValidationResult(errorMessageFormat);
                 }
-                if (input.Length > 10)
-                {
-                    return new ValidationResult(errorMessageLength);
-                }
-                if(input.Length != 10 || !input.All(char.IsDigit))
-                {
-                    return new ValidationResult(errorMessageFormatNumber);
-                }
-
             }
             return ValidationResult.Success;
         }
